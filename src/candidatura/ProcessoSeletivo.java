d863f3b06bcbe74fc2d76aa8226f239eb41f5c67
bericktk <br.ericktk@gmail.com>
2025-02-24 22:11:36 -0300

    Realizando a seleção de candidatos!

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,12 +1,32 @@
 package candidatura;
-import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        analisarCandidato(1900.0);
-        analisarCandidato(2200.0);
-        analisarCandidato(2000.0);
+        selecaoCandidatos();
+    }
 
+    static void selecaoCandidatos(){
+        String [] candidatos = {"Felipe", "João", "Maria", "José", "Ana"};
+
+        int candidatosSelecionados = 0;
+        int candidatosAtuais = 0;
+        double salarioBase = 2000.0;
+        while (candidatosSelecionados < 5) {
+            String candidato = candidatos[candidatosAtuais];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " Solicitou este valor de salário:" + salarioPretendido);
+            if(salarioBase >= salarioPretendido){
+                System.out.println("O Candidato " + candidato + " foi selecionado para a vaga");
+                candidatosSelecionados++;
+            }
+            candidatosAtuais++;
+        }
+    }
+
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
     }
 
     static void analisarCandidato(double salarioPretendido){
