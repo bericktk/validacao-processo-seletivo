f99c13141b8a226f12fe7a0281e066e65db9c6ef
bericktk <br.ericktk@gmail.com>
2025-02-24 22:33:43 -0300

    Adiciona lógica para entrar em contato com candidatos e registrar tentativas

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,9 +1,39 @@
 package candidatura;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        imprimirSelecionados();
+        String [] candidatos = {"Felipe", "Marcia", "Julia", "João", "Maria"};
+        for(String candidato: candidatos){
+            entrandoEmContato(candidato);
+        }
+    }
+
+    static void entrandoEmContato(String candidato){
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+        
+        do{
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if(continuarTentando){
+                tentativasRealizadas++;
+            }else{
+                System.out.println("Contato Realizado com Sucesso!");
+            }
+        }while(continuarTentando && tentativasRealizadas < 3);
+        
+        if(atendeu){
+            System.out.println("Conseguimos contato com " + candidato + " Na " + tentativasRealizadas + " tentativa");
+        }else{
+            System.out.println("Não conseguimos contato com " + candidato + " Após " + tentativasRealizadas + " tentativas");
+        }
+    }
+
+    static boolean atender(){
+        return new Random().nextInt(3)==1;
     }
 
     static void imprimirSelecionados(){
